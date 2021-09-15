67ed0906ec370f306cee4d70e2395c4fa13f8013
ThallesP <contato@thalles.me>
2021-09-15 16:08:47 -0300

    :memo: Add Swagger documentation

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
-
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/users", usersRoutes);
 
 export { app };
